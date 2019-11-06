45bfe79ec8573572f8af2481dcff4055efb7943b
pmadamed <pawel.merdala@adamed.com.pl>
2019-11-06 18:03:27 +0100

    Dodaj pliki projektów.

---
+++ ProgrammaticDiscoverableService/CalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using ServiceContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammaticDiscoverableService
+{
+    public class CalculatorService : ICalculator
+    {
+        int ICalculator.Add(int a, int b) => Add(a, b);
+
+        protected int Add(int a, int b) => a + b;
+    }
+}
---
+++ ServiceContract/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContract
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        int Add(int a, int b);
+    }
+}
